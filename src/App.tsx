48010be6bc50ece5619e6710bce440a7814cdf01
Yurii Novikov <urichcoolx@gmail.com>
2023-08-31 00:21:48 +0200

    added-base-styles

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppBackground from './components/App-background/AppBackground';
+
+function App() {
+  return (
+    <div className="App">
+<AppBackground/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/App-background/AppBackground.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const AppBackground:FC = () => {
+    return <div className="app-background-day"></div>;
+}
+
+export default AppBackground;
