25e43f956b55b2b365f14c4d40811e96919cce9e
Yurii Novikov <urichcoolx@gmail.com>
2025-02-23 00:33:19 +0100

    redeploy

--- src/redux/app/operations.ts
+++ src/redux/app/operations.ts
@@ -1,14 +1,14 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios, { AxiosError, AxiosResponse } from "axios";
 
-const quoteBASE_URL: string = "http://api.quotable.io/";
+const quoteBASE_URL: string = "https://zenquotes.io/api/";
 
 export const fetchQuote = createAsyncThunk(
   "fetchQuote",
   async (): Promise<{ quote: string; author: string }> => {
     try {
       const res: AxiosResponse = await axios.get(`${quoteBASE_URL}random`);
-      return { quote: res.data.content, author: res.data.author };
+      return { quote: res.data[0].q, author: res.data[0].a };
     } catch(e) {
       console.error((e as AxiosError).message);
       return { quote: "Something went wrong please try again", author: "" };
